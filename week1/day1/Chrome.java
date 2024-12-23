36ea0c20069b914c2c33b70d3f454a279b25f8bf
KomathyGanesh <komathy1994@gmail.com>
2024-12-23 14:19:37 +0530

    Add files via upload
    Week1 programs & Assigments

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version=91.0f;
+		String developer="Google";
+		boolean Beta=false;
+		int releaseYearOfValue=2008;
+		char shortcutKey='C';
+	System.out.println("Version of Type is "+version);
+	System.out.println("Developer name is "+developer);
+	System.out.println("Beta is "+Beta);
+	System.out.println("Release Year of value is "+releaseYearOfValue);
+	System.out.println("Shortcut Key is "+shortcutKey);
+
+
+	}
+
+}
---
+++ week1/day2/LearnIfCondition.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class LearnIfCondition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int costOfMobile=500;
+		
+			if (costOfMobile<500)
+			{
+				System.out.println("The delivery is free");
+			}
+			else
+			{
+			
+				System.out.println("The delivery charge is 100 ");
+		}
+
+	}
+}
